58a188db766ad78f35719a2d396241503258eab5
kunal hande <kunalhand30@gmail.com>
2025-06-15 12:08:02 +0530

    Initial commit

---
+++ .history/README_20250615115907.md
@@ -0,0 +1,5 @@
+#mongodb
+saving the data in the mongodb 
+
+#mongoose 
+use mongoose library to make connection between the mongodb and node.js
---
+++ .history/index_20250615115411.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+main().then(() =>{
+    console.log("connection successful");
+})
+.catch((err) =>console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+const userSchema = new mongoose.Schema({
+    name:  String,
+    email: String,
+    age: Number,
+});
+
+const User = mongoose.model("User",userSchema);
+
+// const user2 = new User({
+//     name : "Ram",
+//     email : "ram@gmail.com",
+//     age : 40,
+// });
+
+// user2.save()
+//    .then((res)=>{
+//        console.log(res)
+//    }).catch((err) =>{
+//        console.log(err)   
+//    });
+
+user.insertMany([
+    {name:"ram",eamil:"ram@gmail.com",age:50},
+    {name:"om",eamil:"om@gmail.com",age:40},
+    {name:"joy",eamil:"joy@gmail.com",age:30},
+    {name:"rock",eamil:"rock@gmail.com",age:60},
+]).then((data) =>{
+    console.log(data)
+});
