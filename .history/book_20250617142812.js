55e3d6e2ac0014cf778aecc868dc1cba4a11d0a6
kunal hande <kunalhand30@gmail.com>
2025-06-17 15:10:33 +0530

    its updation of mongoose CRUD

---
+++ .history/book_20250617142812.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+main().then(() =>{
+    console.log("connection successful");
+})
+.catch((err) =>console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+}
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require:true,
+    },
+    author: {
+        type:String
+    },
+     price:{
+       type :Number
+    },
+     discount:{
+        type:Number,
+        default: 0, 
+     },
+     category: {
+        type: String,
+        enum : ["fiction", "non-fiction"]
+     }
+});
+
+const book = mongoose.model("book",bookSchema);
+
+let book1 = new book({  
+    title:"Marvel comics",
+    author:"john",
+    price:500,
+    disccount:50,
+    category:"fiction",
+});
+
+book1.save()
+.then((res) => {
+    console.log(res)
+}).catch((err) =>{
+    console.log(err)
+});
---
+++ .history/book_20250617145850.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+main().then(() =>{
+    console.log("connection successful");
+})
+.catch((err) =>console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+}
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require:true,
+    },
+    author: {
+        type:String
+    },
+     price:{
+       type :Number,
+       min: 1,
+    },
+     discount:{
+        type:Number,
+        default: 0, 
+     },
+     category: {
+        type: String,
+        enum : ["fiction", "non-fiction"]
+     }
+});
+
+const book = mongoose.model("book",bookSchema);
+
+book.findByIdAndUpdate("Marvel comics",{price:-100}, {runValidators: true})
+.then((res) =>{
+    console.log(res)
+})
+  .catch((err) =>{
+    console.log(err.errors.price.property.message);
+  });
+
+// let book1 = new book({  
+//     title:"Marvel comics",
+//     author:"john",
+//     price:500,
+//     disccount:50,
+//     category:"fiction",
+// });
+
+// book1.save()
+// .then((res) => {
+//     console.log(res)
+// }).catch((err) =>{
+//     console.log(err)
+// });
